9e2352eb4c8ca5299ceec1b0ca0ee014e03ed91d
shubham <shubham_ug22@nsut.ac.in>
2025-09-29 21:35:22 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+// Go requires every variable you declare to be used somewhere in your code. If you comment out all uses of a variable, you'll get an error until you use it again or remove the declaration.
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int{
+  return x+y
+  }
+
+func main(){
+  sum := add(5,3)
+  fmt.Println(sum)
+  }
